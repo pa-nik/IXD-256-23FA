ef2f815548975964bd89af7613b2bbfb8cd982a6
Nikita Pashenkov <nikitapashenkov@gmail.com>
2023-10-25 08:42:26 -0700

    read 2 values with p5

--- class06/webserial_pyscript_p5/main.py
+++ class06/webserial_pyscript_p5/main.py
@@ -1,24 +1,34 @@
 import js as p5
 from js import document
 
-data = None
+data_string = None
+data_list = None
+sensor_val = None
 
 def setup():
   p5.createCanvas(300, 300)
-  print('hello p5!')
 
 def draw():
   p5.background(255)
-
-  global data
-  data = document.getElementById("data").innerText
+  global data_string, data_list, sensor_val
   
-  circle_size = int(data)
-  p5.noStroke()
-  p5.fill(150)
-  p5.ellipse(150, 150, circle_size, circle_size)
+  # assign content of "data" div on index.html page to variable: 
+  data_string = document.getElementById("data").innerText
+  # split data_string by comma, making a list:
+  data_list = data_string.split(',')
 
-def print_test(x):
-  print(x)
+  # assign first item of data_list to sensor_val:
+  sensor_val = int(data_list[0])
+
+  # draw circle responding to sensor data:
+  p5.push()  # save transformation coordinates
+  # move coordinates by (x, y):
+  p5.translate(75, 75)
+  p5.noStroke()  # disable stroke
+  p5.fill(0)  # black fill
+  # draw ellipse with (x, y, width, height): 
+  p5.ellipse(0, 0, sensor_val, sensor_val)
+  p5.push()  # restore transformation coordinates
+  
 
   
