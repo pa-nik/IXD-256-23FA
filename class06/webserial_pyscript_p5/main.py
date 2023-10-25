427d2cf47222a5dffe5c88b343eb0409c531fe59
Nikita Pashenkov <nikitapashenkov@gmail.com>
2023-10-25 09:02:40 -0700

    add square color responding to sensor

--- class06/webserial_pyscript_p5/main.py
+++ class06/webserial_pyscript_p5/main.py
@@ -7,6 +7,8 @@ sensor_val = None
 
 def setup():
   p5.createCanvas(300, 300)
+  # change mode to draw rectangles from center:
+  p5.rectMode(p5.CENTER)
 
 def draw():
   p5.background(255)
@@ -20,12 +22,17 @@ def draw():
   # assign first item of data_list to sensor_val:
   sensor_val = int(data_list[0])
 
-  # draw circle responding to sensor data:
-  p5.push()  # save transformation coordinates
-  # move coordinates by (x, y):
-  p5.translate(75, 75)
   p5.noStroke()  # disable stroke
+  # fill function can take 1 argument (gray)
   p5.fill(0)  # black fill
-  # draw ellipse with (x, y, width, height):
-  p5.ellipse(0, 0, sensor_val, sensor_val)
-  p5.push()  # restore transformation coordinates
+  
+  # draw circle changing size with sensor data:
+  # ellipse function takes (x, y, width, height)
+  p5.ellipse(75, 75, sensor_val, sensor_val)
+  
+  # draw square changing color with sensor data:
+  # fill function can take (red, blue, green)
+  p5.fill(sensor_val, 0, 255 - sensor_val)  
+  # rectangle function takes (x, y, width, height)
+  p5.rect(225, 75, 100, 100)
+  
